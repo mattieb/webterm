3cba66bb13c99b0f7e55ed0b3da67d89492d56c0
Mattie <mattie@zigg.com>
2023-08-31 22:15:37 -0400

    stop Electron from popping errors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { buildApplicationMenu } from "./main/menu";
 import { newWindow } from "./main/new-window";
 import { setupIpc } from "./main/ipc";
 
+process.on('uncaughtException', console.error);
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 // if (require("electron-squirrel-startup")) {
 //   app.quit();
