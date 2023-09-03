5a44b260c9d3cb38a1a939a245a1c34c29dc8d71
Mattie <mattie@zigg.com>
2023-09-03 12:58:35 -0400

    get locale at runtime; fix trailing space bug

--- src/main/shell-config.ts
+++ src/main/shell-config.ts
@@ -1,9 +1,10 @@
-import { basename } from 'node:path';
+import { app } from "electron";
+import { basename } from "node:path";
 
 export const getShellConfig = () => {
   const file = process.env.SHELL ?? "/bin/sh";
   const argv0 = `-${basename(file)}`;
-  const env = { LANG: "en_US.UTF-8 " };
+  const env = { LANG: `${app.getLocale()}.UTF-8` };
 
   return { file, argv0, env };
 };
