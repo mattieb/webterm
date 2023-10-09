dd9883701050deee6623730ce4c166a1c0fbd227
Mattie <mattie@mattiebee.io>
2023-10-09 19:51:32 -0400

    refactor renderer

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,13 +1,7 @@
-import "xterm/css/xterm.css";
 import "./index.css";
-import { Terminal } from "xterm";
 import type { MainApi } from "./preload";
-import { FitAddon } from "xterm-addon-fit";
-import { TerminalDimensions } from "./common/types";
-
-const terminalOptions = {
-  fontFamily: "SF Mono",
-};
+import { initializeTerminal } from "./renderer/initialize-terminal";
+import "xterm/css/xterm.css";
 
 declare global {
   interface Window {
@@ -16,34 +10,5 @@ declare global {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const terminalElement = document.getElementById("terminal");
-
-  const terminal = new Terminal(terminalOptions);
-
-  const fitAddon = new FitAddon();
-  terminal.loadAddon(fitAddon);
-
-  terminal.open(terminalElement);
-
-  const resize = () => {
-    const dimensions: TerminalDimensions = fitAddon.proposeDimensions();
-    fitAddon.fit();
-    window.main.resize(dimensions);
-  };
-  window.addEventListener("resize", resize);
-
-  terminal.onData((data) => window.main.data(data));
-  terminal.onTitleChange((title) => (document.title = title));
-
-  window.main.onData((data) => {
-    terminalElement.removeAttribute("disabled");
-    terminal.write(data);
-  });
-
-  window.main.onExit(() => terminalElement.setAttribute("disabled", "true"));
-
-  window.main.onReady(() => {
-    resize();
-    terminal.focus();
-  });
+  initializeTerminal(document.getElementById("terminal"));
 });
