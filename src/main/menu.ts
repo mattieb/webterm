0de217989e39ce2dfa8d784edd914f6e344ee622
Mattie <mattie@mattiebee.io>
2025-03-06 13:18:38 -0500

    improve Windows menus

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -3,22 +3,38 @@ import { Menu } from "electron";
 const isMac = process.platform === "darwin";
 
 export const buildApplicationMenu = (createWindow: () => void) =>
-  Menu.buildFromTemplate([
-    { role: "appMenu" },
-    {
-      label: "File",
-      submenu: isMac
-        ? [
+  isMac
+    ? Menu.buildFromTemplate([
+        { role: "appMenu" },
+        {
+          label: "File",
+          submenu: [
             {
               label: "New Terminal",
               accelerator: "Command+N",
               click: createWindow,
             },
             { role: "close" },
-          ]
-        : [{ role: "quit" }],
-    },
-    { role: "editMenu" },
-    { role: "viewMenu" },
-    { role: "windowMenu" },
-  ]);
+          ],
+        },
+        { role: "editMenu" },
+        { role: "viewMenu" },
+        { role: "windowMenu" },
+      ])
+    : Menu.buildFromTemplate([
+        {
+          label: "File",
+          submenu: [
+            {
+              label: "New Terminal",
+              accelerator: "Ctrl+N",
+              click: createWindow,
+            },
+            { role: "close" },
+            { role: "quit" },
+          ],
+        },
+        { role: "editMenu" },
+        { role: "viewMenu" },
+        { role: "windowMenu" },
+      ]);
